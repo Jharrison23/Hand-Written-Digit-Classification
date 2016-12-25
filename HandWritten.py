0d8ea214c59a5ad2ff1713e4247ab57bf9c5902e
jharrison23 <jharrison23@knights.ucf.edu>
2016-12-24 23:15:11 -0500

    Initial commit, imported dataset and stored in variable

--- HandWritten.py
+++ HandWritten.py
@@ -1,1 +1,8 @@
 
+
+# import MNIST
+# The input_data class is a standard python class that downloads the dataset
+# splits it into training and testing data, and formats it for use later on
+import input_data
+
+mnist = input_data.read_data_sets("/tmp/data", one_hot=True)
