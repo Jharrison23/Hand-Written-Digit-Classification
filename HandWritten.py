f1212b6fb4b49fb4a6a35b7bb5ccc03cb05d40f1
jharrison23 <jharrison23@knights.ucf.edu>
2016-12-25 00:05:40 -0500

    Lots of comments explaining the model and created and explained the inputs and outputs

--- HandWritten.py
+++ HandWritten.py
@@ -23,3 +23,28 @@ training_iteration = 30
 batch_size = 100
 
 display_step = 2
+
+
+# Create our model
+# in tensorflow this model is represented as a dataflow graph, the graph contains
+# a set of nodes called operations, these are units of computations ranging in
+# difficulty from addition to multivariable equations, each operations takes as input
+# a tensor and ourputs a tensor
+
+# A tensor is a multi dimensional array and flow between operations, this is how
+# data is represented in tensor flow
+
+# Start building our model by creating two operations
+
+# placeholder operations are just variables which we assign data to at a later date
+# they are never initialize and doesnt contain data,
+
+# we will define the the type and shape of our data as the parameters
+# variable x: Input images represented by a 2d tensor of numbers, 784 is the dimensionalality
+# of a single flattened mnist image, flattening an image is when you convert a 2d array
+# to a 1d array by unstacking the rows and lining them up 28 * 28 = 784
+x = tf.placeholder("float", [none, 784])
+
+#output classes y: consist of a 2d tensor, where each row is a one_hot ten dimensional
+# vector showing which digit class the corresponding mnist digit corresponds to 
+y = tf.placeholder("float", [none, 10])
