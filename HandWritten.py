60a166908f5623b36692d559f2fd0584a93ff229
jharrison23 <jharrison23@knights.ucf.edu>
2016-12-25 13:09:15 -0500

    added header comments

--- HandWritten.py
+++ HandWritten.py
@@ -1,4 +1,9 @@
+# Machine learning program written in python which buids a handwritten digit image classifier
+# and trains this classifier to recognize images in the MNIST digit dataset.
+# uses logistic regression as its model and will test and train on the mnist dataset
+# tensorboard can then be used to view the constructed data
 
+# This was written by James Harrison following along with an open source tutorial
 
 # import MNIST
 # The input_data class is a standard python class that downloads the dataset
