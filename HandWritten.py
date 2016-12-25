3c2dd56ccad8ade0ec5660620267809d03ad0186
jharrison23 <jharrison23@knights.ucf.edu>
2016-12-24 23:37:03 -0500

    Imported Tensorflow as well as set hyper parameters

--- HandWritten.py
+++ HandWritten.py
@@ -6,3 +6,20 @@
 import input_data
 
 mnist = input_data.read_data_sets("/tmp/data", one_hot=True)
+
+import tensorflow as tf
+
+# Set Hyperparameters represent tuning knobs for the model
+
+# Learning rate: Defines how fast we want to update our weights
+# if this is too small our model might need too many iterations to converge on
+# the best results, if its it too big the model might skip the optimal solution
+# 0.01 is a known good learning rate for this particular problem
+learning_rate = 0.01
+
+# the number of iterations used for training
+training_iteration = 30
+
+batch_size = 100
+
+display_step = 2
